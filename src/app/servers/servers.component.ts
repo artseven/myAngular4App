57cc0667d0fff57412837762870651266a163a34
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-06-28 17:53:35 -0400

    2 components added

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
+  selector: 'app-server',
   template: `
     <app-server></app-server>
     <app-server></app-server>`,
